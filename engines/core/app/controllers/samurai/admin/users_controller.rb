f66793b8e461f608bff95584b2ecbedf021ff5e4
thuankg1752 <thuankg1752@gmail.com>
2020-08-11 11:16:14 +0700

    admin page

---
+++ engines/core/app/controllers/samurai/admin/users_controller.rb
@@ -0,0 +1,9 @@
+module Samurai
+    module Admin
+      class UsersController < AdminController
+        def index
+          @users = Samurai::User.ordered.limit(3)
+        end
+      end
+    end
+end
